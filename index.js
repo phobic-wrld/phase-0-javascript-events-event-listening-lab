c91eab7f6b1b2a55ca304f39ffe8b87d347b7c3e
joy <blackbearmode1@gmail.com>
2024-04-01 00:07:13 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
 }
+
+function() {
+    alert('I was clicked!');
+  }
+
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+
+
